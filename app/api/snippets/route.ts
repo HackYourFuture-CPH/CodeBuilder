e171baa797b1d7cfe00be903aa8facec58c2209c
juliadavydenko <julietta12345@yahoo.com>
2023-06-02 19:07:34 +0200

    API endpoint for all snippets, works on http://localhost:3000/api/snippets

---
+++ app/api/snippets/route.ts
@@ -0,0 +1,111 @@
+// task1 - list of all snippets
+
+/* this is how I started, to check that it works:
+
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request): Promise<NextResponse> {
+  return NextResponse.json({ name: 'API route for all Snippets List' });
+}
+*/
+
+
+//Then I used route.ts file from tags folder as a reference
+
+import { getMongoDb } from "@/app/mongodb";
+import { NextResponse } from "next/server";
+
+export interface Snippet {
+    id: string;
+    title: string;
+    description: string;
+    favoritedBy: string[];
+    tags: string[];
+    code: string;
+    created_at: Date;
+    updated_at: Date;
+    author_id: string;
+}
+
+export async function GET(req: Request): Promise<NextResponse> {
+  const snippetsFromDatabase = await getMongoDb()
+    .collection("snippets")
+    .find({})
+    .toArray();
+
+  if (!snippetsFromDatabase.length) {
+    await getMongoDb().collection("snippets").insertMany(snippets);
+    return NextResponse.json(snippets);
+  }
+
+  return NextResponse.json(snippetsFromDatabase);
+}
+
+const snippets: Omit<Snippet, "_id">[] = [
+  {
+     _id: "1ghjhkkhj",
+    title: "Snippet1",
+    description: "Snippet1 description",
+    favoritedBy: [],
+    tags: [],
+    code: "Snippet1 code",
+    created_at: '2023-06-02 18:17:35',
+    updated_at: '2023-06-02 18:19:35',
+    author_id: "Snippet1 author",
+  },
+  { 
+    _id: "2ghdfgfhfhj",
+    title: "Snippet2",
+    description: "Snippet2 description",
+    favoritedBy: [],
+    tags: [],
+    code: "Snippet2 code",
+    created_at: '2023-06-02 18:17:35',
+    updated_at: '2023-06-02 18:19:35',
+    author_id: "Snippet2 author",
+  },
+  {
+     _id: "3gdfggdfgdjhkkhj",
+     title: "Snippet3",
+    description: "Snippet3 description",
+    favoritedBy: [],
+    tags: [],
+    code: "Snippet3 code",
+    created_at: '2023-06-02 18:17:35',
+    updated_at: '2023-06-02 18:19:35',
+    author_id: "Snippet3 author",
+  },
+  {
+     _id: "4ghjhksdfdsfdskhj",
+     title: "Snippet4",
+    description: "Snippet4 description",
+    favoritedBy: [],
+    tags: [],
+    code: "Snippet4 code",
+    created_at: '2023-06-02 18:17:35',
+    updated_at: '2023-06-02 18:19:35',
+    author_id: "Snippet4 author",
+  },
+  {
+     _id: "5ghdfsderjhkkhj",
+    title: "Snippet5",
+    description: "Snippet5 description",
+    favoritedBy: [],
+    tags: [],
+    code: "Snippet5 code",
+    created_at: '2023-06-02 18:17:35',
+    updated_at: '2023-06-02 18:19:35',
+    author_id: "Snippet5 author",
+  },
+];
+
+/*
+Due to the fact, that other routes go to this same file, they can be extended here:
+// one snippet by id
+
+// delete snippet by id
+
+Other questions:
+~Dynamic/Static? [id] or the same file?
+~Should we have pages instead of app folder?
+*/
