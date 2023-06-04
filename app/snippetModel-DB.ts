94d1ba999feea9f42b67cefada25d9dc5123d860
Ramanzada <zadaray99@gmail.com>
2023-06-04 15:41:12 +0200

    We changed the file name of our Database to snippetModel-DB.ts but the name of the branch is still the same

---
+++ app/snippetModel-DB.ts
@@ -0,0 +1,11 @@
+export interface snippetModel {
+  _id: string;
+  title: string;
+  description: string;
+  favoriteByIds: string[];
+  tags: string[];
+  snippetCode: string;
+  createdAt: Date;
+  updatedAt: Date;
+  authorId: string;
+}
