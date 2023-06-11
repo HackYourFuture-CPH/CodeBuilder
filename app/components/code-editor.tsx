58b6e4f3c5110f69940cf5daa6a555f7e7033e89
Mima <mimmamenser@yahoo.com>
2023-06-11 13:58:31 +0200

    Monaco Editor was created

---
+++ app/components/code-editor.tsx
@@ -0,0 +1,56 @@
+import MonacoEditor from '@monaco-editor/react';
+
+interface CodeEditorProps {
+    initialValue: string;
+    readOnly?: boolean;
+    tags: string[];
+}
+
+// Define the type of LanguageMap object
+interface LanguageMap {
+    [key: string]: string;
+}
+
+const getLanguageFromTags = (tags: string[]): string => {
+    // Map the tags to language identifiers
+    const languageMap: { [key: string]: string } = {
+        HTML: 'html',
+        CSS: 'css',
+        JAVASCRIPT: 'javascript',
+        GIT: 'plaintext',
+        SQL: 'sql',
+    };
+
+    // Get the languageMap of the first element of tags array
+    const language = languageMap[tags[0]];
+
+    // Either return the mapped language or plaintext
+    return language ? language : 'plaintext';
+};
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, readOnly, tags }) => {
+    const language = getLanguageFromTags(tags);
+
+    const options = {
+        wordWrap: "on" as "on",
+        fontSize: 16,
+        automaticLayout: true,
+        readOnly: readOnly,
+        padding: {
+            top: 16,
+            bottom: 16,
+        }
+    };
+
+    return (
+        <MonacoEditor
+            value={initialValue}
+            height="500px"
+            language={language}
+            theme="vs-dark"
+            options={options}
+        />
+    );
+};
+
+export default CodeEditor;
--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,34 +1,35 @@
 "use client";
 import useSWR from "swr";
 import styles from "./page.module.css";
-import { Tag } from "../api/tags/route";
+import { Snippet } from "../api/snippets/route";
+import CodeEditor from "../components/code-editor";
 
 export default function TagsPage() {
-  const { data: tags } = useSWR<Tag[]>("/api/tags", async (url) => {
-    const response = await fetch(url);
-    return response.json();
-  });
+    const { data: snippets } = useSWR<Snippet[]>("/api/snippets", async (url) => {
+        try {
+            const response = await fetch(url);
+            const dataSnippets = await response.json();
+            return dataSnippets;
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
-
-      <h1>Categories from the database</h1>
-      <h2>This is the main page (user not logged in)</h2>
-
-      <div className={styles.grid}>
-        {tags?.map((tag) => (
-          <div className={styles.card} key={tag.shortName}>
-            <h2>{tag.displayName}</h2>
-            <p>{tag.shortName}</p>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+    return (
+        <main className={styles.main}>
+            {snippets?.map((snippet) => (
+                <div key={snippet._id} style={{ width: "50%", margin: '0 auto', borderRadius: "5px" }}>
+                    <CodeEditor
+                        initialValue={snippet.snippetCode}
+                        readOnly={true}
+                        tags={snippet.tags}
+                    />
+                    <div style={{ backgroundColor: 'white', color: 'black', paddingBottom: '20px', marginBottom: "30px", padding: "20px" }}>
+                        <h2 style={{ marginTop: 0 }}>{snippet.title}</h2>
+                        <p>{snippet.description}</p>
+                    </div>
+                </div>
+            ))}
+        </main>
+    );
 }
