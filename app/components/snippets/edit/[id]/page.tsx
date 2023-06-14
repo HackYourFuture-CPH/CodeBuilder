18787dc89957de89415fab3bebd650cf31e46767
juliadavydenko <julietta12345@yahoo.com>
2023-06-14 17:44:00 +0200

    used SWR to fetch the snippet data from the API endpoint

--- app/components/snippets/edit/[id]/page.tsx
+++ app/components/snippets/edit/[id]/page.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import { useState } from "react";
 import SnippetForm from "../../snipetForm/snippetForm";
 import { SnippetData } from "./interfaces";
+import useSWR from "swr";
+import { Tag } from "@/app/api/tags/route";
+import SelectTags from "../../SelectTags";
 
 const EditSnippet = ({ params }: { params: { id: string } }) => {
   //use SWR to fetch the snipet with id params.id
-  const [title, setTitle] = useState<string>("");
-  const [tags, setTags] = useState<string[]>([]);
-  const [description, setDescription] = useState<string>("");
-  const [code, setCode] = useState<string>("");
+
+  const { data: snippetData, error } = useSWR(`/api/snippets/${params.id}`);
+const [title, setTitle] = useState<string>(snippetData?.title || "");
+const [tags, setTags] = useState<string[]>(snippetData?.tags || []);
+const [description, setDescription] = useState<string>(
+  snippetData?.description || ""
+);
+const [code, setCode] = useState<string>(snippetData?.code || "");
   const [selectTags, setSelectTags] = useState<string[]>([]);
 
   const handlePublish = (): void => {
@@ -20,7 +27,23 @@ const EditSnippet = ({ params }: { params: { id: string } }) => {
       code: code,
     };
 
-    //fetch snippets edit route
+    fetch("/api/snippets/${params.id}", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(snippetData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Snippet updated!");
+        } else {
+          throw new Error("Error updating the snippet");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
