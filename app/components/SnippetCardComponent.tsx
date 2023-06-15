0c7c242dd53ed6e81fe2ca7a6d06b914e0c6534f
slastushevskyi <slastushevskyi@gmail.com>
2023-06-15 12:30:11 +0200

    created simple output

--- app/components/SnippetCardComponent.tsx
+++ app/components/SnippetCardComponent.tsx
@@ -1,5 +1,17 @@
 "use client"
+
 const SnippetCardComponent = ({title,description, favorite, tags, code, created_at, updated_at, author_id}) => {
+    return(
+<div>
+    <p>{title}</p>
+    <p>{description}</p>
+    <p>{favorite}</p>
+    <p>{tags}</p>
+    <p>{code}</p>
+    <p>{created_at}</p>
+    <p>{updated_at}</p>
+    <p>{author_id}</p>
+</div>    )
 console.log(title,description, favorite, tags, code, created_at, updated_at, author_id)
 };
 
