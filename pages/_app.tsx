41154eb025314d8a22849b3cfbeb4acfd59dea79
Mima <mimmamenser@yahoo.com>
2023-06-16 14:43:24 +0200

    Merge conflict resolved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,10 @@
-/*import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <SessionProvider session={pageProps.session}>
             <Component {...pageProps} />
-        </SessionProvider>
-    );
-}
-
-export default MyApp;*/
-
-import { SessionProvider } from "next-auth/react";
-import { AppProps } from "next/app";
-
-function MyApp({ Component, pageProps }: AppProps) {
-    const { session, ...restPageProps } = pageProps;
-
-    return (
-        <SessionProvider session={session}>
-            <Component {...restPageProps} />
         </SessionProvider>
     );
 }
