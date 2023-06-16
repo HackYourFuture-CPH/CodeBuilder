30d0c93e64a7132ec9307bbff4a9a9cae10e4f8a
slastushevskyi <slastushevskyi@gmail.com>
2023-06-16 11:45:56 +0200

    exmample layout

--- app/components/SnippetCardComponent.tsx
+++ app/components/SnippetCardComponent.tsx
@@ -1,19 +1,27 @@
-"use client"
+"use client";
 
-const SnippetCardComponent = ({title,description, favorite, tags, code, created_at, updated_at, author_id}) => {
-    return(
-<div>
-    <p>{code}</p>
-    <h1>{title}</h1>
-    <p>{description}</p>
-    <p>{favorite}</p>
-    <p>{tags}</p>
-    
-    <p>{created_at}</p>
-    <p>{updated_at}</p>
-    <p>{author_id}</p>
-</div>    )
-console.log(title,description, favorite, tags, code, created_at, updated_at, author_id)
+const SnippetCardComponent = ({
+  title,
+  description,
+  favoriteByIds,
+  tags,
+  snippetCode,
+  createdAt,
+  updatedAt,
+  authorId,
+}: any) => {
+  return (
+    <div>
+      <p>{snippetCode}</p>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p>{favoriteByIds}</p>
+      <p>{tags}</p>
+      <p>{createdAt}</p>
+      <p>{updatedAt}</p>
+      <p>{authorId}</p>
+    </div>
+  );
 };
 
 export default SnippetCardComponent;
