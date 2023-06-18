6d14a2702466284ac1a7b2dc4902b46d678cc15a
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-06-18 12:43:09 +0200

    modified:   app/snippets/page.tsx

--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+import useSWR from 'swr'
+import { getSnippets } from '../services/SnippetService';
+import { snippetModel } from '../snippetModel-DB';
+
+
 const UserId: React.FC = () => {
+  const { data: snippets } = useSWR<snippetModel[]>('/api/snippets', getSnippets);
+
   return (
-    <div>
-      This component(route) we will use as the page after the user logs in Here
-      we gonna show all snippets, created by logged in user
-    </div>
+    <>
+      <p>
+        This component(route) we will use as the page after the user logs in Here
+        we gonna show all snippets, created by logged in user
+      </p>
+      <ul>
+        {snippets ? 
+          snippets.map((snippet: any) => (
+            <li key={snippet._id}>
+              <h2>{snippet.title}</h2>
+              <p>{snippet._id}</p>
+              <p>{snippet.createdAt.slice(0, 10)}</p>
+              <p>{snippet.snippetCode}</p>
+            </li>
+          ))
+          : 
+          <div>Loading...</div>
+        }
+      </ul>
+       
+    </>
   );
 };
 export default UserId;
