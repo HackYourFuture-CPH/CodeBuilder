72371597f74f9ffd9eb9c4de3a9cb1713df3c201
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-06-18 13:00:54 +0200

    new file:   app/snippets/[id]/edit/page.tsx

---
+++ app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import {  mutate } from 'swr';
+import { updateSnippet } from '@/app/services/SnippetService';
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+const EditSnippet: React.FC<Props> = ({params: {id}}) => {
+
+    const newTitle = {
+        title: 'NEWTEST'
+    };
+
+    const handleClick = async () => {
+        
+        const updatedSnippet = await updateSnippet(`/api/snippets/`, id, newTitle);
+
+        mutate(`/api/snippets/${id}`, updatedSnippet, {
+            optimisticData: (snippet: any) => ({ ...snippet, title: newTitle }),
+            rollbackOnError: true
+          });
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Change!</button>
+        </div>
+    )
+}
+
+export default EditSnippet;
