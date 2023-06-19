8ac2d3632b9087f6a62ccc91def4b2b17b7a60a1
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-06-19 18:21:31 +0200

    Changed files app/snippets/[id]/edit/page.tsx, app/snippets/[id]/page.tsx, app/snippets/page.tsx for mutation get snippet by Id

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,4 +1,41 @@
-const SnippetsId: React.FC = () => {
-  return <div>Here we have snippets by id</div>;
+"use client";
+import useSWR from "swr";
+import { getSnippets } from "../../services/SnippetService";
+import { snippetModel } from "../../snippetModel-DB";
+import Link from "next/link";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const SnippetsId: React.FC<Props> = ({ params: { id } }) => {
+  const { data: snippets } = useSWR<snippetModel>(
+    `/api/snippets/${id}`,
+    getSnippets
+  );
+
+  console.log(snippets);
+  return (
+    <>
+      <div>Here we have snippets by id</div>
+      {snippets ? (
+        <div>
+          <h1>{snippets.title}</h1>
+          <p>{snippets.description}</p>
+          <p>{snippets.snippetCode}</p>
+          <p>{snippets.createdAt}</p>
+          <p>{snippets.updatedAt}</p>
+          <p>{snippets.authorId}</p>
+          <Link href={`/snippets/${id}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
 };
 export default SnippetsId;
