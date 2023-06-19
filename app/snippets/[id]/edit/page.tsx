6c541424d791f1b6672c878103dc2a827288460f
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-06-19 16:16:08 +0200

    Formated file  app/snippets/[id]/edit/page.tsx

--- app/snippets/[id]/edit/page.tsx
+++ app/snippets/[id]/edit/page.tsx
@@ -1,34 +1,32 @@
-'use client'
-import {  mutate } from 'swr';
-import { updateSnippet } from '@/app/services/SnippetService';
+"use client";
+import { mutate } from "swr";
+import { updateSnippet } from "@/app/services/SnippetService";
 
 type Props = {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 };
 
-const EditSnippet: React.FC<Props> = ({params: {id}}) => {
+const EditSnippet: React.FC<Props> = ({ params: { id } }) => {
+  const newTitle = {
+    title: "NEWTEST",
+  };
 
-    const newTitle = {
-        title: 'NEWTEST'
-    };
+  const handleClick = async () => {
+    const updatedSnippet = await updateSnippet(`/api/snippets/`, id, newTitle);
 
-    const handleClick = async () => {
-        
-        const updatedSnippet = await updateSnippet(`/api/snippets/`, id, newTitle);
+    mutate(`/api/snippets/${id}`, updatedSnippet, {
+      optimisticData: (snippet: any) => ({ ...snippet, title: newTitle }),
+      rollbackOnError: true,
+    });
+  };
 
-        mutate(`/api/snippets/${id}`, updatedSnippet, {
-            optimisticData: (snippet: any) => ({ ...snippet, title: newTitle }),
-            rollbackOnError: true
-          });
-    }
-
-    return (
-        <div>
-            <button onClick={handleClick}>Change!</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={handleClick}>Change!</button>
+    </div>
+  );
+};
 
 export default EditSnippet;
