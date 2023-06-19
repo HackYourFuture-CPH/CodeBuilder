10d080705646ad569a8b388a9a3b8669833ec807
Shweta-MG <shweta.malav@gmail.com>
2023-06-19 21:33:03 +0200

    Page.tsx file is updated to import BurgerMenu component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import useSWR from "swr";
 import styles from "./page.module.css";
 import { Tag } from "./api/tags/route";
+import BurgerMenu from "./api/components/shared/burgerMenu/BurgerMenu";
 
 export default function Home() {
   const { data: tags } = useSWR<Tag[]>("/api/tags", async (url) => {
@@ -10,24 +11,28 @@ export default function Home() {
   });
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
+    <div className={styles.main}>
+      <BurgerMenu />
 
-      <h1>Categories from the database</h1>
 
-      <div className={styles.grid}>
-        {tags?.map((tag) => (
-          <div className={styles.card} key={tag.shortName}>
-            <h2>{tag.displayName}</h2>
-            <p>{tag.shortName}</p>
-          </div>
-        ))}
-      </div>
-    </main>
+      {
+        /*
+        <h1>Categories from the database</h1>
+        
+    
+  
+        
+        <div className={styles.grid}>
+          {tags?.map((tag) => (
+            <div className={styles.card} key={tag.shortName}>
+              <h2>{tag.displayName}</h2>
+              <p>{tag.shortName}</p>
+            </div>
+          ))}
+        </div>
+         */
+      }
+
+    </div>
   );
 }
