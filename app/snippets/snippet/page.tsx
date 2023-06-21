43354ea95f8253f10cfa2679fe06284ef56ddbdb
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-06-21 18:28:32 +0200

    created snippet details component

---
+++ app/snippets/snippet/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import useSWR from "swr";
+import { getSnippets, updateSnippet } from "../../services/SnippetService";
+import { snippetModel } from "../../snippetModel-DB";
+import CodeEditor from "../../components/shared/codeEditor/code-editor";
+import UserIcon from "@/app/icons/user";
+
+interface RouteParams {
+  id: string;
+}
+
+const SnippetDetails: React.FC = () => {
+  const params = useParams<RouteParams>();
+  const { id } = params;
+  console.log(id);
+
+  const { data: snippet } = useSWR<snippetModel>(
+    `/api/snippets/${id}`,
+    getSnippets
+  );
+
+  console.log(snippet);
+
+  const addToFavorite = (authorId: string) => {
+    snippet?.favoriteByIds.push(authorId);
+    const updatedSnippet = {
+      ...snippet,
+    };
+
+    if (snippet?.favoriteByIds.includes(authorId)) {
+      return console.log("it already include");
+    }
+    updateSnippet(`/api/snippet/${id}`, id, updatedSnippet);
+    console.log("done ");
+  };
+
+  const normalizeDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "short" });
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year}`;
+  };
+
+  return (
+    <>
+      {snippet ? (
+        <div>
+          <div>
+            <div>
+              <h1>{snippet.title}</h1>
+              <ul>
+                {snippet.tags.map((tag: string) => (
+                  <li key={tag}>{tag}</li>
+                ))}
+              </ul>
+              <p>{snippet.description}</p>
+            </div>
+            <div>
+              <div>
+                {" "}
+                <Link href={`/snippet/${id}/edit`}>
+                  <button type="button">Edit</button>
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => addToFavorite(snippet.authorId)}
+                >
+                  ❤️
+                  {/* here will be heart icon */}
+                </button>
+              </div>
+              <UserIcon />
+              <p>
+                {snippet.authorId} {normalizeDate(snippet.updatedAt)}
+              </p>
+            </div>
+          </div>
+          <CodeEditor
+            initialValue={snippet.snippetCode}
+            readOnly="true"
+            tags={snippet.tags}
+          />
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
+};
+export default SnippetDetails;
