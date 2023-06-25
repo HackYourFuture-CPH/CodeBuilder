7e0468ef40b8f9b7634aeea1815f73a30e476898
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-06-25 12:55:39 +0200

    Added required changes

--- app/components/shared/header/loginBtn/login-btn.tsx
+++ app/components/shared/header/loginBtn/login-btn.tsx
@@ -1,23 +1,23 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import "./login-btn.css";
-import { FaUserAlt } from 'react-icons/fa';
+import { FaUserAlt } from "react-icons/fa";
 
 export default function LoginBtn() {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-    return (
-        <>
-            {/* Logout or Login */}
-            <FaUserAlt className="icon" /><button
-                className={"login-btn"}
-                onClick={(e) => {
-                    e.preventDefault();
-                    session?.user ? signOut() : signIn();
-                }}
-            >
-                <span>{session?.user ? session.user.name : "Login"}</span>
-            </button>
-        </>
-    );
-
+  return (
+    <>
+      {/* Logout or Login */}
+      <button
+        className={"login-btn"}
+        onClick={(e) => {
+          e.preventDefault();
+          session?.user ? signOut() : signIn();
+        }}
+      >
+        <FaUserAlt className="icon" />
+        <span>{session?.user ? session.user.name : "Login"}</span>
+      </button>
+    </>
+  );
 }
--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,7 +1,6 @@
 import Header from "../components/shared/header/header";
 
 const Explore: React.FC = () => {
-  return <div>
-    Here we gonna return some snippets to explore.</div>;
+  return <div>Here we gonna return some snippets to explore.</div>;
 };
 export default Explore;
