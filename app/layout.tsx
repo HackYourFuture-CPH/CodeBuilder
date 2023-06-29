2e85c0671b368c87845ccf408334fb50bc1eec56
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-06-29 23:11:37 +0200

    Created footer and responsive design for it.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import Header from "./components/shared/header/header";
 import Providers from "./providers";
+import Footer from "./components/shared/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <Header />
           {children}
         </Providers>
+        <Footer />
       </body>
     </html>
   );
