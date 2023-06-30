18b3db4b4fc238182fb45c8a9466753e50e770b0
Shweta-MG <shweta.malav@gmail.com>
2023-06-30 08:21:06 +0200

    Branch on branch created.

---
+++ app/components/shared/tags/tags.tsx
@@ -0,0 +1,11 @@
+import './tags.module.css';
+
+const Tags = () => {
+    return ( 
+        <div>
+            <h1>Here the filtered tags will be rendered??</h1>
+        </div>
+     );
+}
+ 
+export default Tags;
