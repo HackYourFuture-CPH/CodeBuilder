a283c5f68e3fec566b63cea3c518453d758e6851
Shweta-MG <shweta.malav@gmail.com>
2023-07-02 00:29:39 +0200

    Changes are being updated.

--- app/components/shared/ApplyFilterButton/ApplyFilterButton.tsx
+++ app/components/shared/ApplyFilterButton/ApplyFilterButton.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import './ApplyFilterButton.css'
 
 export default function BasicButtons() {
   return (
-    <Stack spacing={2} direction="row">
+    <Stack spacing={2} direction="row" className='ApplyFilterButton'>
       <Button variant="contained">Apply Filter</Button>
     </Stack>
   );
-}
+};
