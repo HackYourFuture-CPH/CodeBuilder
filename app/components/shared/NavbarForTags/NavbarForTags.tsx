a833a79edb29c92913f6da099f8316d3c642a4ff
Shweta-MG <shweta.malav@gmail.com>
2023-07-01 23:24:36 +0200

    Folder structure for tag filter and filter navbar is being created.

---
+++ app/components/shared/NavbarForTags/NavbarForTags.tsx
@@ -0,0 +1,13 @@
+import './NavbarForTags.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SnippetGallery from '../tags/Tag';
+
+const NavbarForTags = () => {
+    return (<div>
+        navbar is being created here for tags and search bar!!
+        <SearchBar />
+        <SnippetGallery/>
+    </div> );
+}
+ 
+export default NavbarForTags;
---
+++ app/components/shared/SearchBar/SearchBar.tsx
@@ -0,0 +1,9 @@
+import './SearchBar.css'
+
+const SearchBar = () => {
+    return ( <div>
+        Search bar is created here!!!
+    </div> );
+}
+ 
+export default SearchBar;
