868af13d20c57251a4bfb905d2f3233fa51ebc2b
Shweta-MG <shweta.malav@gmail.com>
2023-07-02 00:47:59 +0200

    Compliance as per npm build.

--- app/components/shared/ApplyFilterButton/ApplyFilterButton.tsx
+++ app/components/shared/ApplyFilterButton/ApplyFilterButton.tsx
@@ -6,7 +6,9 @@ import './ApplyFilterButton.css'
 export default function BasicButtons() {
   return (
     <Stack spacing={2} direction="row" className='ApplyFilterButton'>
-      <Button variant="contained">Apply Filter</Button>
+      <button className="button" type="submit">
+        Apply filter
+      </button>
     </Stack>
   );
 };
