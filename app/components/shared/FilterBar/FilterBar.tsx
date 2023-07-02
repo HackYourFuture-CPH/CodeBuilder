97002e1663114f5eaef6269c6482694b0f91101e
Shweta-MG <shweta.malav@gmail.com>
2023-07-02 22:01:50 +0200

    Filter is being created for tags and snippets.

--- app/components/shared/FilterBar/FilterBar.tsx
+++ app/components/shared/FilterBar/FilterBar.tsx
@@ -5,23 +5,26 @@ import SelectTags from "../../snippets/snipetForm/SelectTags";
 import { Tag } from "@/app/api/tags/route";
 import { useState } from "react";
 import { snippetModel } from "@/app/snippetModel-DB";
-import SnippetGallery from "../../SnippetsGallery";
+//import SnippetGallery from "../../SnippetsGallery";
 
 export interface Option {
   label: string;
   value: string;
 }
 
-const FilterBar = (snippets: snippetModel[]) => {
+const FilterBar = ({ snippets }: { snippets: snippetModel[] }) => {
+
+
   const [selectTags, setSelectTags] = useState<any[]>([]);
   const [queryTitle, setQueryTitle] = useState<string>("");
   const [filteredSnippets, setFilteredSnippets] = useState<snippetModel[]>([]);
-  console.log("selectTags", selectTags);
-  console.log(" queryTitle", queryTitle);
+ // console.log("selectTags", selectTags);
+  //console.log(" queryTitle", queryTitle);
+  //console.log(snippets);
+
 
   const { data: tags } = useSWR<Tag[]>("/api/tags", async (url) => {
     const response = await fetch(url);
-
     return response.json();
   });
 
@@ -31,16 +34,20 @@ const FilterBar = (snippets: snippetModel[]) => {
       label: tag.displayName,
     })) || [];
 
+
+
   const handlerSubmit = () => {
     const snippetsAfterFilter = snippets?.filter((snippet: snippetModel) => {
-      snippet.tags.some((tag) =>
-        selectTags.map((tag) => tag.value).includes(tag)
-      ) || snippet.title.includes(queryTitle);
+      return snippet.tags.some((tag) =>
+        selectTags.map(tags => tags.value.trim().toLowerCase()).includes(tag.trim().toLowerCase())
+      ) || snippet.title.includes(queryTitle.trim().toLowerCase());
     });
     console.log(snippetsAfterFilter);
-
     return setFilteredSnippets(snippetsAfterFilter);
   };
+
+  
+
   // filtered_snippets = snippets.filter(snippet => snippet.tags.some(tag => tags.map(tag => tag.title).includes(tag))).map(snippet => );
   return (
     <div className="FilterBar_Container">
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import React from "react";
-import CreateSnippet from "./components/snippets/create/page";
 
 const Home: React.FC = () => {
   return (
     <div className="main">
-      <CreateSnippet />
     </div>
   );
 };
