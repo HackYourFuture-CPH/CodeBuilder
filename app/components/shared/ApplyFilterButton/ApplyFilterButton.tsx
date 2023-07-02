f6ac9cb22f6fce50a48809d5b6842437684b43f6
Shweta-MG <shweta.malav@gmail.com>
2023-07-02 08:47:55 +0200

    Add filter button is created and CSS corrected.

--- app/components/shared/ApplyFilterButton/ApplyFilterButton.tsx
+++ app/components/shared/ApplyFilterButton/ApplyFilterButton.tsx
@@ -1,14 +1,9 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React from 'react';
 import './ApplyFilterButton.css'
 
-export default function BasicButtons() {
-  return (
-    <Stack spacing={2} direction="row" className='ApplyFilterButton'>
-      <button className="button" type="submit">
+export default function ApplyFilterButton() {
+  return ( <button className="button" type="submit">
         Apply filter
       </button>
-    </Stack>
   );
 };
--- app/components/shared/NavbarForTags/NavbarForTags.tsx
+++ app/components/shared/NavbarForTags/NavbarForTags.tsx
@@ -1,14 +1,16 @@
 import './NavbarForTags.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SnippetGallery from '../tags/Tag';
-import BasicButtons from '../ApplyFilterButton/ApplyFilterButton';
+import ApplyFilterButton from '../ApplyFilterButton/ApplyFilterButton';
 
 const NavbarForTags = () => {
     return (<div className='NavbarForTags'>
         navbar is being created here for tags and search bar!!
+        <div className='TagsDisplay'>
         <SearchBar />
+        <ApplyFilterButton />
+        </div>
         <SnippetGallery />
-        <BasicButtons />
     </div> );
 }
  
