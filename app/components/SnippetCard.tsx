22f37015c29ea675cf8656c23a8607247e753523
slastushevskyi <slastushevskyi@gmail.com>
2023-07-02 13:35:52 +0200

    userId hidden from frontend side, created snippets/\[id\]/favorite api route

--- app/components/SnippetCard.tsx
+++ app/components/SnippetCard.tsx
@@ -31,15 +31,15 @@ const SnippetCard = ({
   mutate,
 }: SnippetCardModel) => {
   const { data: session } = useSession();
-  const userId: any = session?.user?.email;
+  const userId: any = session?.user?.id;
 
   const handleFavoriteButton = async () => {
-    await fetch(`/api/snippets/${snippet._id}`, {
+    await fetch(`/api/snippets/${snippet._id}/favorite`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ addToFavorite: userId }),
+      // body: JSON.stringify({ addToFavorite: "id" }),
     })
       .then((response) => {
         if (response.ok) {
