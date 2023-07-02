8633af90f177e61124f5215826e52f56c0ee815f
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-07-02 16:55:37 +0200

    Content added to home page.
    •Added img
    •Added text and some csss.
    This PR is not ready to be reviewed yet. Design is not finished.

--- app/components/shared/footer/footer.tsx
+++ app/components/shared/footer/footer.tsx
@@ -1,8 +1,14 @@
 "use client";
 import React from "react";
 import "./footer.css";
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
+  const pathname = usePathname()
+  const showFooter = pathname != "/"
+  if (!showFooter) {
+    return null
+  }
   return (
     <footer className="footer">
       <div className="footer-container">
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 import React from "react";
+import Image from "next/image";
+import "./page.css";
 
 const Home: React.FC = () => {
-  
   return (
-    <div className='main'>
+    <div className="main">
+      <div className="home-container">
+        <div className="typewriter">
+          <h1> Sharing Code, <br></br>Shaping Futures</h1>
+        </div>
+        <a href="/explore">
+          <button className="start-btn">Start now</button>
+        </a>
+      </div>
+      <Image
+        src="/images/StudentsCoding.png"
+        alt="Students-coding"
+        width={400}
+        height={400}
+      />
     </div>
   );
 };
