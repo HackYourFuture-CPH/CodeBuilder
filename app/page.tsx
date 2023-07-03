ab77c66db26ff2e7aa46ec8782bdda0c53b4563e
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-07-03 16:15:00 +0200

    Added background gif

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import "./page.css";
 
 const Home: React.FC = () => {
   return (
-    <div
-      className="background"
-      style={{ backgroundImage: 'url("/public/images/Background.gif")' }}
-    >
-      <div className="main">
-        <div className="home-container">
-          <div className="typewriter">
-            <h1> Sharing Code, Shaping Futures</h1>
-          </div>
-          <a href="/explore">
-            <button className="start-btn">Start now</button>
-          </a>
-        </div>
+    <div className="main">
+    <div className="background-container">
+      <div>
+        <Image src="/images/Background.gif" alt="Background" width={494} height={119} />
       </div>
     </div>
+    <div className="home-container">
+      <div className="typewriter">
+        <h1>Sharing Code, Shaping Futures</h1>
+      </div>
+      <a href="/explore">
+        <button className="start-btn">Start now</button>
+      </a>
+    </div>
+  </div>
   );
 };
 
