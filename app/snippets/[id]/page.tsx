86b0e41a41a37e9ac4ab7a47bfc336a2c8f6e437
AnB801 <antony.bondarev@gmail.com>
2023-07-03 15:52:38 +0200

    add styling for snippetsId Page

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,65 +1,68 @@
-"use client";
-import React from "react";
-import Link from "next/link";
-import useSWR from "swr";
-import { getSnippets } from "../../services/SnippetService";
-import { useSession } from "next-auth/react";
-import { snippetModel } from "../../snippetModel-DB";
-import CodeEditor from "../../components/shared/codeEditor/code-editor";
-import { addToFavorite, normalizeDate } from "./handlers";
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import useSWR from 'swr'
+import { getSnippets } from '../../services/SnippetService'
+import { useSession } from 'next-auth/react'
+import { snippetModel } from '../../snippetModel-DB'
+import CodeEditor from '../../components/shared/codeEditor/code-editor'
+import { addToFavorite, normalizeDate } from './handlers'
+import './page.module.css'
 
 export default function SnippetDetails({ params }: { params: { id: string } }) {
-  const id = params.id;
+  const id = params.id
   const { data: snippet } = useSWR<snippetModel>(
     `/api/snippets/${id}`,
     getSnippets
-  );
-  const { data: session } = useSession();
-  const userId = session?.user?.email?.toString();
+  )
+  const { data: session } = useSession()
+  const userId = session?.user?.email?.toString()
 
   return (
     <>
       {snippet ? (
-        <div>
-          <div>
-            <div>
+        <div className="container">
+          <div className="title-container">
+            <div className="snippets-title">
               <h1>{snippet.title}</h1>
-              <ul>
-                {snippet?.tags?.map((tag: string) => (
-                  <li key={tag}>{tag}</li>
-                ))}
-              </ul>
-              <p>{snippet.description}</p>
             </div>
-            <div>
-              <div>
-                {" "}
+            <div className="buttons">
+              <li>
+                {' '}
                 <Link href={`/snippet/${id}/edit`}>
-                  <button type="button">Edit</button>
+                  <button type="button" className="edit-button">
+                    Edit
+                  </button>
                 </Link>
+              </li>
+              <li>
                 <button
+                  className="like-button"
                   type="button"
                   onClick={() =>
-                    addToFavorite(id, snippet, userId ? userId : "")
+                    addToFavorite(id, snippet, userId ? userId : '')
                   }
                 >
                   ❤️
                 </button>
-              </div>
-              <p>
-                {snippet.authorId} {normalizeDate(new Date(snippet.updatedAt))}
-              </p>
+              </li>
             </div>
           </div>
-          <CodeEditor
-            initialValue={snippet.snippetCode}
-            readOnly={true}
-            tags={snippet.tags}
-          />
+
+          <div className="snippet-description">
+            <p>{snippet.description}</p>
+          </div>
+          <div className="code-snippet">
+            <CodeEditor
+              initialValue={snippet.snippetCode}
+              readOnly={true}
+              tags={snippet.tags}
+            />
+          </div>
         </div>
       ) : (
         <div>Loading...</div>
       )}
     </>
-  );
+  )
 }
