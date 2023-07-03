d3b0cd4c8f5e75bd7dd3e0081bdf603ebf6b0d34
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-07-03 12:46:47 +0200

    Changes to style added

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import "./page.css";
 
 const Home: React.FC = () => {
   return (
-    <div className="main">
-      <div className="home-container">
-        <div className="typewriter">
-          <h1> Sharing Code, Shaping Futures</h1>
+    <div
+      className="background"
+      style={{ backgroundImage: 'url("/public/images/Background.gif")' }}
+    >
+      <div className="main">
+        <div className="home-container">
+          <div className="typewriter">
+            <h1> Sharing Code, Shaping Futures</h1>
+          </div>
+          <a href="/explore">
+            <button className="start-btn">Start now</button>
+          </a>
         </div>
-        <a href="/explore">
-          <button className="start-btn">Start now</button>
-        </a>
-      </div>
-      <div>
-        <Image
-          src="/images/Background.gif"
-          alt="background"
-          width={400}
-          height={400}
-        />
       </div>
     </div>
   );
