dbefb5ca50b0f30e1c10ba8dfb3c831559f508ab
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-07-03 17:31:14 +0200

    Added the image in css instead of using next/image

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,19 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import "./page.css";
 
 const Home: React.FC = () => {
   return (
     <div className="main">
-    <div className="background-container">
       <div>
-        <Image src="/images/Background.gif" alt="Background" width={494} height={119} />
+        <div className="typewriter">
+          <h1>Sharing Code, Shaping Futures</h1>
+        </div>
+        <a href="/explore">
+          <button className="start-btn">Start now</button>
+        </a>
       </div>
     </div>
-    <div className="home-container">
-      <div className="typewriter">
-        <h1>Sharing Code, Shaping Futures</h1>
-      </div>
-      <a href="/explore">
-        <button className="start-btn">Start now</button>
-      </a>
-    </div>
-  </div>
   );
 };
 
