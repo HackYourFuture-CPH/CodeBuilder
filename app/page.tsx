ae83d8b9f6c0e48377fa2c30f500d05620af7513
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-07-04 20:31:55 +0200

    Added <Link>

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 import React from "react";
 import "./page.css";
+import Link from "next/link";
 
 const Home: React.FC = () => {
   return (
     <div className="main">
-       <video className="video-background" autoPlay loop muted>
+      <video className="video-background" autoPlay loop muted>
         <source src="/videos/Background.mp4" type="video/mp4" />
       </video>
       <div className="home-container">
         <div className="typewriter">
           <h1>Sharing Code, Shaping Futures</h1>
         </div>
-        <a href="/snippets">
+        <Link href="/snippets">
           <button className="start-btn">Start now</button>
-        </a>
+        </Link>
       </div>
     </div>
   );
