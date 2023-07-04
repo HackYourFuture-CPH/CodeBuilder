761af528e5e7b8786bc17431d4e74ac8bb6c0a2c
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-07-04 22:41:27 +0200

    Added tags and more css.

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,34 +1,37 @@
-'use client'
-import React from 'react'
-import Link from 'next/link'
-import useSWR from 'swr'
-import { getSnippets } from '../../services/SnippetService'
-import { useSession } from 'next-auth/react'
-import { snippetModel } from '../../snippetModel-DB'
-import CodeEditor from '../../components/shared/codeEditor/code-editor'
-import { addToFavorite } from './handlers'
-import './page.css'
+"use client";
+import React from "react";
+import Link from "next/link";
+import useSWR from "swr";
+import { getSnippets } from "../../services/SnippetService";
+import { useSession } from "next-auth/react";
+import { snippetModel } from "../../snippetModel-DB";
+import CodeEditor from "../../components/shared/codeEditor/code-editor";
+import { addToFavorite } from "./handlers";
+import "./page.css";
+import TagsPage from "@/app/tags/page";
 
 export default function SnippetDetails({ params }: { params: { id: string } }) {
-  const id = params.id
+  const id = params.id;
   const { data: snippet } = useSWR<snippetModel>(
     `/api/snippets/${id}`,
     getSnippets
-  )
-  const { data: session } = useSession()
-  const userId = session?.user?.email?.toString()
+  );
+  const { data: session } = useSession();
+  const userId = session?.user?.email?.toString();
 
   return (
     <>
       {snippet ? (
         <div className="container">
-          <div className="title-container">
+          <div className="details-container">
             <div className="snippets-title">
               <h1>{snippet.title}</h1>
+              <p className="tags">{snippet.tags}</p>
+              <p>{snippet.description}</p>
             </div>
             <div className="buttons">
               <li>
-                {' '}
+                {" "}
                 <Link href={`/snippet/${id}/edit`}>
                   <button type="button" className="edit-button">
                     Edit
@@ -40,16 +43,13 @@ export default function SnippetDetails({ params }: { params: { id: string } }) {
                   className="like-button"
                   type="button"
                   onClick={() =>
-                    addToFavorite(id, snippet, userId ? userId : '')
+                    addToFavorite(id, snippet, userId ? userId : "")
                   }
                 >
                   ❤️
                 </button>
               </li>
             </div>
-          </div>
-          <div className="snippet-description">
-            <p>{snippet.description}</p>
           </div>
           <div className="code-snippet">
             <CodeEditor
@@ -63,5 +63,5 @@ export default function SnippetDetails({ params }: { params: { id: string } }) {
         <div className="loading">Loading...</div>
       )}
     </>
-  )
+  );
 }
