9ce996cc98b3977538fbdca80dec1fce6aaeacd0
Dima-Kutaini <deemaKutainy@gmail.com>
2023-07-05 19:14:54 +0200

    After Thomas review

--- app/components/SnippetCard.tsx
+++ app/components/SnippetCard.tsx
@@ -1,17 +1,22 @@
-/* eslint-disable @next/next/no-img-element */
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
+
 /** @format */
 
-"use client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
+'use client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(faHeart);
-import Link from "next/link";
-import CodeEditor from "./shared/codeEditor/code-editor";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-import "./snippetCard.css";
-import { snippetModel } from "../snippetModel-DB";
+import Link from 'next/link';
+import CodeEditor from './shared/codeEditor/code-editor';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import './snippetCard.css';
+import { snippetModel } from '../snippetModel-DB';
 
 export interface SnippetCardModel {
   snippet: snippetModel;
@@ -31,16 +36,16 @@ const SnippetCard = ({ snippet, formatDate, mutate }: SnippetCardModel) => {
 
   const handleFavoriteButton = async () => {
     await fetch(`/api/snippets/${snippet._id}/favorite`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error("Error publishing snippet");
+          throw new Error('Error publishing snippet');
         }
       })
       .catch((error) => {
@@ -63,18 +68,17 @@ const SnippetCard = ({ snippet, formatDate, mutate }: SnippetCardModel) => {
         {session && (
           <button
             className="favorite-button"
-            onClick={() => handleFavoriteButton()}
-          >
+            onClick={() => handleFavoriteButton()}>
             {userId && snippet.favoriteByIds.includes(userId) ? (
               <FontAwesomeIcon
                 icon={faHeart}
-                style={{ color: "#ff0000" }}
+                style={{ color: '#ff0000' }}
                 size="2xl"
               />
             ) : (
               <FontAwesomeIcon
                 icon={faHeart}
-                style={{ color: "#000000" }}
+                style={{ color: '#000000' }}
                 size="2xl"
               />
             )}
@@ -91,7 +95,9 @@ const SnippetCard = ({ snippet, formatDate, mutate }: SnippetCardModel) => {
         <div className="tags-container">
           <div className="button-container">
             {snippet.tags.map((item) => (
-              <p className="tags" key={item}>
+              <p
+                className="tags"
+                key={item}>
                 {item}
               </p>
             ))}
@@ -103,27 +109,30 @@ const SnippetCard = ({ snippet, formatDate, mutate }: SnippetCardModel) => {
             <div className="img-container">
               <img
                 src={snippet.authorImage}
-                alt=" pic"
+                alt=" "
                 width={40}
                 height={40}
-                style={{ borderRadius: "50%", objectFit: "cover" }}
+                style={{ borderRadius: '50%', objectFit: 'cover' }}
               />
             </div>
             <p
               className="avatar-text"
               style={{
-                margin: "0",
-              }}
-            >
+                margin: '0',
+              }}>
               by {snippet.author} {formatDate(new Date(snippet.createdAt))}
             </p>
           </div>
           {session ? (
-            <Link className="link-button" href={`/snippets/${snippet._id}`}>
+            <Link
+              className="link-button"
+              href={`/snippets/${snippet._id}`}>
               Learn more
             </Link>
           ) : (
-            <Link className="link-button" href={`/login`}>
+            <Link
+              className="link-button"
+              href={`/login`}>
               Login to learn more
             </Link>
           )}
